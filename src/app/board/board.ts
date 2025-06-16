a7482991ea12bfc29d41fa44c93942f6f4e9cd85
Diana Veselkina <vess_di@mail.ru>
2025-06-16 23:49:14 +0300

    add style

--- src/app/board/board.ts
+++ src/app/board/board.ts
@@ -3,13 +3,21 @@ import { Header } from '../header/header';
 import { Counter } from '../counter/counter';
 import { Button } from '../button/button';
 import { Triangle } from '../triangle/triangle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
-  imports: [Header, Counter, Button, Triangle],
+  imports: [Header, Counter, Button, Triangle, CommonModule ],
   templateUrl: './board.html',
   styleUrl: './board.css'
 })
 export class Board {
-
+style1 = { backgroundColor: 'red', width: '2rem', height: '2rem'}; // первый стиль
+style2 = { backgroundColor: 'blue', width: '3rem', height: '3rem' }; // второй стиль
+style3 = { backgroundColor: 'green', width: '1rem', height: '1rem' }; // третий стиль
+style4 = { backgroundColor: 'red', width: '4rem', height: '4rem' }; // первый стиль
+style5 = { backgroundColor: 'blue', width: '2rem', height: '2rem' }; // второй стиль
 }
+
+
+
--- src/app/triangle/triangle.ts
+++ src/app/triangle/triangle.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-triangle',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './triangle.html',
   styleUrl: './triangle.css'
 })
 export class Triangle {
-
+ @Input() dataFromParent: any;
 }
+
+
+  
