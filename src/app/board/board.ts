2bef18d63411db2dfa9e07e5eed23033d17f2dbf
Diana Veselkina <vess_di@mail.ru>
2025-06-17 15:54:56 +0300

    cgange position triangle

--- src/app/board/board.ts
+++ src/app/board/board.ts
@@ -15,8 +15,8 @@ export class Board {
 style1 = { backgroundColor: 'red', width: '2rem', height: '2rem'}; // первый стиль
 style2 = { backgroundColor: 'blue', width: '3rem', height: '3rem' }; // второй стиль
 style3 = { backgroundColor: 'green', width: '1rem', height: '1rem' }; // третий стиль
-style4 = { backgroundColor: 'red', width: '4rem', height: '4rem' }; // первый стиль
-style5 = { backgroundColor: 'blue', width: '2rem', height: '2rem' }; // второй стиль
+style4 = { backgroundColor: 'yellow', width: '4rem', height: '4rem' }; // первый стиль
+style5 = { backgroundColor: 'black', width: '2rem', height: '2rem' }; // второй стиль
 }
 
 
