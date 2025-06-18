656cc1ff6aa61fd291fe32dd8a7ee54091a6541b
Diana Veselkina <vess_di@mail.ru>
2025-06-18 21:51:55 +0300

    triangle removal change

--- src/app/board/board.ts
+++ src/app/board/board.ts
@@ -14,11 +14,16 @@ import { Circle } from '../circle/circle';
 })
 export class Board {
   counter: number = 0;
-style1 = { cursor: 'pointer', backgroundColor: 'red', width: '2rem', height: '2rem'}; // первый стиль
-style2 = { cursor: 'pointer', backgroundColor: 'blue', width: '3rem', height: '3rem' }; // второй стиль
-style3 = { cursor: 'pointer', backgroundColor: 'green', width: '1rem', height: '1rem' }; // третий стиль
-style4 = { cursor: 'pointer', backgroundColor: 'yellow', width: '4rem', height: '4rem' }; // первый стиль
-style5 = { cursor: 'pointer', backgroundColor: 'black', width: '2rem', height: '2rem' }; // второй стиль
+style1 = { cursor: 'pointer', backgroundColor: 'red', width: '2rem', height: '2rem'}; 
+style2 = { cursor: 'pointer', backgroundColor: 'blue', width: '3rem', height: '3rem' }; 
+style3 = { cursor: 'pointer', backgroundColor: 'green', width: '1rem', height: '1rem' }; 
+style4 = { cursor: 'pointer', backgroundColor: 'yellow', width: '4rem', height: '4rem' }; 
+style5 = { cursor: 'pointer', backgroundColor: 'black', width: '5rem', height: '5rem' }; 
+style6 = { cursor: 'pointer', backgroundColor: 'pink', width: '7rem', height: '7rem'}; 
+style7 = { cursor: 'pointer', backgroundColor: 'grey', width: '3.5rem', height: '3.5rem' }; 
+style8 = { cursor: 'pointer', backgroundColor: 'violet', width: '1rem', height: '1rem' }; 
+style9 = { cursor: 'pointer', backgroundColor: 'orange', width: '8rem', height: '8rem' }; 
+style10 = { cursor: 'pointer', backgroundColor: 'gold', width: '2rem', height: '2rem' }; 
 
 circles: {id:number; visible:boolean; color:string; size:number; borderRadius?: number | string; position?: string; top?: number | string; left?: number | string;}[] = [
   { id: 1, visible: true, color: 'red', size: 100, borderRadius: '50%', position: 'absolute', top: '10rem', left: '12rem' },
@@ -40,6 +45,11 @@ circles: {id:number; visible:boolean; color:string; size:number; borderRadius?: 
   showTriangle3: boolean = true;
   showTriangle4: boolean = true;
   showTriangle5: boolean = true;
+  showTriangle6: boolean = true;
+  showTriangle7: boolean = true;
+  showTriangle8: boolean = true;
+  showTriangle9: boolean = true;
+  showTriangle10: boolean = true;
 
   handleTriangleClick(triangleNumber: number): void {
     switch (triangleNumber) {
@@ -61,18 +71,49 @@ circles: {id:number; visible:boolean; color:string; size:number; borderRadius?: 
           this.counter++;
         }
         break;
-      case 4:
+         case 4:
         if (this.showTriangle4) {
           this.showTriangle4 = false;
           this.counter++;
         }
         break;
-      case 5:
+        case 5:
         if (this.showTriangle5) {
           this.showTriangle5 = false;
           this.counter++;
         }
         break;
+        case 6:
+        if (this.showTriangle6) {
+          this.showTriangle6 = false;
+          this.counter++;
+        }
+        break;
+        case 7:
+        if (this.showTriangle7) {
+          this.showTriangle7 = false;
+          this.counter++;
+        }
+        break;
+        case 8:
+        if (this.showTriangle8) {
+          this.showTriangle8 = false;
+          this.counter++;
+        }
+        break;
+        case 9:
+        if (this.showTriangle9) {
+          this.showTriangle9 = false;
+          this.counter++;
+        }
+        break;
+      case 10:
+        if (this.showTriangle10) {
+          this.showTriangle10 = false;
+          this.counter++;
+        }
+        break;
+     
     }
   }
 }
