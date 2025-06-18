3069c6881f30b88327cf3ba491cd677a69f3eb51
Diana Veselkina <vess_di@mail.ru>
2025-06-18 17:51:35 +0300

    add style circle

--- src/app/board/board.ts
+++ src/app/board/board.ts
@@ -4,10 +4,11 @@ import { Counter } from '../counter/counter';
 import { Button } from '../button/button';
 import { Triangle } from '../triangle/triangle';
 import { CommonModule } from '@angular/common';
+import { Circle } from '../circle/circle';
 
 @Component({
   selector: 'app-board',
-  imports: [Header, Counter, Button, Triangle, CommonModule ],
+  imports: [Header, Counter, Button, Triangle, CommonModule, Circle ],
   templateUrl: './board.html',
   styleUrl: './board.css'
 })
@@ -19,8 +20,21 @@ style3 = { cursor: 'pointer', backgroundColor: 'green', width: '1rem', height: '
 style4 = { cursor: 'pointer', backgroundColor: 'yellow', width: '4rem', height: '4rem' }; // первый стиль
 style5 = { cursor: 'pointer', backgroundColor: 'black', width: '2rem', height: '2rem' }; // второй стиль
 
+circles: {id:number; visible:boolean; color:string; size:number; borderRadius?: number | string; position?: string; top?: number | string; left?: number | string;}[] = [
+  { id: 1, visible: true, color: 'red', size: 100, borderRadius: '50%', position: 'absolute', top: '10rem', left: '12rem' },
+  { id: 2, visible: true, color: 'green', size: 70, borderRadius: '50%', position: 'absolute', top: '16rem', left: '7rem' },
+  { id: 3, visible: true, color: 'blue', size: 60, borderRadius: '50%', position: 'absolute', top: 14, left: '5rem' }
+    
+  ];
 
   
+  handleCircleClick(id: number): void {
+    const circle = this.circles.find(t => t.id === id);
+    if (circle && circle.visible) {
+      circle.visible = false;
+      
+    }
+  }
   showTriangle1: boolean = true;
   showTriangle2: boolean = true;
   showTriangle3: boolean = true;
---
+++ src/app/circle/circle.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-circle',
+  imports: [CommonModule],
+  templateUrl: './circle.html',
+  styleUrl: './circle.css'
+})
+export class Circle {
+@Input() visible: boolean = true;
+@Input() styles: { [key: string]: string } = {};
+  @Output() click = new EventEmitter<void>();
+
+  onClick(): void {
+    this.click.emit();
+  }
+}
+
